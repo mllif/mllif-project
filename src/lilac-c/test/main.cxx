ff87fba3f4bda00a2552b7ec1a3d2f7234a42c7c
Sharp0802 <sharp0802.business@gmail.com>
2024-11-12 08:52:27 +0900

    :white_check_mark: Add namespace to test

--- src/lilac-c/test/main.cxx
+++ src/lilac-c/test/main.cxx
@@ -1,12 +1,15 @@
 
-struct __attribute__((lilac_export)) TestStruct {
-  int A;
-  int B;
-  float C;
-};
+namespace TestNS
+{
+    struct __attribute__((lilac_export)) TestStruct {
+        int A;
+        int B;
+        float C;
+    };
 
-int TestFn(TestStruct *T) __attribute__((lilac_export)) {
-  return T->A + T->B + T->C;
+    int TestFn(TestStruct *T) __attribute__((lilac_export)) {
+        return T->A + T->B + T->C;
+    }
 }
 
 int main() {}
