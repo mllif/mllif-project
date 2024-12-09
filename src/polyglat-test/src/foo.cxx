4ba940a79dcbdcae42c7e201fea6d2e70178b618
Sharp0802 <sharp0802.business@gmail.com>
2024-12-09 21:17:03 +0900

    :recycle: Use non-inlined definition

--- src/polyglat-test/src/foo.cxx
+++ src/polyglat-test/src/foo.cxx
@@ -1,12 +1,12 @@
 
 [[polyglat::export]] int i32() { return 0; }
 [[polyglat::export]] int i32_4(int a, int b, int c, int d) { return 0; }
-[[polyglat::export]] int i32_8(int, int, int, int, int, int, int, int) { return 0; }
+[[polyglat::export]] int i32_8(int a, int b, int c, int d, int e, int f, int g, int h) { return 0; }
 
 namespace ns {
     [[polyglat::export]] int i32() { return 0; }
     [[polyglat::export]] int i32_4(int a, int b, int c, int d) { return 0; }
-    [[polyglat::export]] int i32_8(int, int, int, int, int, int, int, int) { return 0; }
+    [[polyglat::export]] int i32_8(int a, int b, int c, int d, int e, int f, int g, int h) { return 0; }
 
     struct [[polyglat::export]] small_object {
         int self;
@@ -14,18 +14,26 @@ namespace ns {
         small_object(){}
         ~small_object(){}
 
-        [[polyglat::export]] small_object i32() { return *this; }
-        [[polyglat::export]] small_object i32_4(int a, int b, int c, int d) { return *this; }
-        [[polyglat::export]] small_object i32_8(int, int, int, int, int, int, int, int) { return *this; }
+        [[polyglat::export]] small_object i32();
+        [[polyglat::export]] small_object i32_4(int a, int b, int c, int d);
+        [[polyglat::export]] small_object i32_8(int a, int b, int c, int d, int e, int f, int g, int h);
     };
+
+    small_object small_object::i32() { return *this; }
+    small_object small_object::i32_4(int a, int b, int c, int d) { return *this; }
+    small_object small_object::i32_8(int a, int b, int c, int d, int e, int f, int g, int h) { return *this; }
 
     struct [[polyglat::export]] large_object {
         int self[999];
 
         large_object(){}
 
-        [[polyglat::export]] large_object i32() { return *this; }
-        [[polyglat::export]] large_object i32_4(int a, int b, int c, int d) { return *this; }
-        [[polyglat::export]] large_object i32_8(int, int, int, int, int, int, int, int) { return *this; }
+        [[polyglat::export]] large_object i32();
+        [[polyglat::export]] large_object i32_4(int a, int b, int c, int d);
+        [[polyglat::export]] large_object i32_8(int a, int b, int c, int d, int e, int f, int g, int h);
     };
+
+    large_object large_object::i32() { return *this; }
+    large_object large_object::i32_4(int a, int b, int c, int d) { return *this; }
+    large_object large_object::i32_8(int a, int b, int c, int d, int e, int f, int g, int h) { return *this; }
 }
