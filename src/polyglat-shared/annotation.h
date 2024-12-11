976a392b88103b148c44ebb1c029ab30217bfbbf
Sharp0802 <sharp0802.business@gmail.com>
2024-12-11 21:20:26 +0900

    :recycle: Refactor annotation target/range

--- src/polyglat-shared/annotation.h
+++ src/polyglat-shared/annotation.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <optional>
 #include <string>
+#include <map>
 
 namespace clang {
-    class NamedDecl;
-    class Attr;
+    class FunctionDecl;
 } // namespace clang
 
 namespace polyglat::shared {
@@ -13,16 +12,10 @@ namespace polyglat::shared {
 
 #define LILAC_DEFINE_ANNOTATION_KEY(value) constexpr std::string value = #value
     // Use string rather than integer code to be human-readable
-    LILAC_DEFINE_ANNOTATION_KEY(NAMESPACE);
+    LILAC_DEFINE_ANNOTATION_KEY(PATH);
     LILAC_DEFINE_ANNOTATION_KEY(NAME);
-    LILAC_DEFINE_ANNOTATION_KEY(SIZE);
-    LILAC_DEFINE_ANNOTATION_KEY(ALIGN);
-    LILAC_DEFINE_ANNOTATION_KEY(CALLCONV);
-    LILAC_DEFINE_ANNOTATION_KEY(RETURN);
-    LILAC_DEFINE_ANNOTATION_KEY(TYPE);
 #undef LILAC_DEFINE_ANNOTATION_KEY
 
-    void MarkAsTarget(clang::NamedDecl *decl);
-    void CreateAnnotation(clang::NamedDecl *decl);
-    auto ParseAnnotation(const std::string &annotation) -> std::optional<std::pair<std::string, std::string>>;
+    void MarkAsTarget(clang::FunctionDecl *decl);
+    void CreateAnnotation(clang::FunctionDecl *decl);
 } // namespace polyglat::shared
