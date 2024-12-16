7ae6a6148d6fb38533d7d88e4fd60c9dc11f04b7
Sharp0802 <sharp0802.business@gmail.com>
2024-12-16 14:59:26 +0900

    :hammer: Use CIR/MLIR pipeline

--- cmake/Polyglat.cmake
+++ cmake/Polyglat.cmake
@@ -1,37 +1,43 @@
 function(add_polyglat_library target language)
     foreach (SOURCE ${ARGN})
-        string(REGEX REPLACE "[.][^.]*$" ".ll" BITCODE "${SOURCE}")
-        string(REGEX REPLACE "^${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" BITCODE "${BITCODE}")
-        set(BITCODES ${BITCODES} "${BITCODE}")
+        string(REGEX REPLACE "[.][^.]*$" ".cir" CIR "${SOURCE}")
+        string(REGEX REPLACE "[.][^.]*$" ".mlir" MLIR "${SOURCE}")
+        string(REGEX REPLACE "[.][^.]*$" ".ll" LL "${SOURCE}")
+        string(REGEX REPLACE "^${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" CIR "${CIR}")
+        string(REGEX REPLACE "^${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" MLIR "${MLIR}")
+        string(REGEX REPLACE "^${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" LL "${LL}")
+        set(CIRS ${CIRS} "${CIR}")
+        set(MLIRS ${MLIRS} "${MLIR}")
+        set(LLS ${LLS} "${LL}")
     endforeach ()
 
     set(COMPILER "CMAKE_${language}_COMPILER")
-    foreach (arg IN ZIP_LISTS BITCODES ARGN)
+    foreach (arg IN ZIP_LISTS CIRS MLIRS LLS ARGN)
+
+        set(CIR "${arg_0}")
+        set(MLIR "${arg_1}")
+        set(LL "${arg_2}")
+        set(INPUT "${arg_3}")
+
         add_custom_command(
-                OUTPUT ${arg_0}
+                OUTPUT ${LL}
                 COMMAND ${${COMPILER}}
                 -fplugin="${POLYGLAT_PATH}/polyglat-c.so"
-                -S -emit-llvm
+                -emit-cir
                 $<TARGET_PROPERTY:${target},COMPILE_OPTIONS>
-                -c "${arg_1}"
-                -o "${arg_0}"
-                DEPENDS ${arg_1}
+                "${INPUT}" -o "${CIR}"
+                COMMAND cir-opt --cir-to-llvm "${CIR}" -o "${MLIR}"
+                COMMAND mlir-translate --mlir-to-llvmir -allow-unregistered-dialect "${MLIR}" > "${LL}"
+                DEPENDS "${INPUT}"
         )
     endforeach ()
 
     set(TARGET_PSM_FILE "$<TARGET_PROPERTY:${target},LIBRARY_OUTPUT_DIRECTORY>/$<TARGET_PROPERTY:${target},PREFIX>${target}.psm")
-    set(ENSEMBLE_BITCODE "$<TARGET_PROPERTY:${target},BINARY_DIR>/$<TARGET_PROPERTY:${target},PREFIX>${target}.ll")
+    set(ENSEMBLE_LL "$<TARGET_PROPERTY:${target},BINARY_DIR>/$<TARGET_PROPERTY:${target},PREFIX>${target}.ll")
     add_custom_target(${target}
-            COMMAND llvm-link ${BITCODES} -o "${ENSEMBLE_BITCODE}"
-            COMMAND opt
-            -load-pass-plugin="${POLYGLAT_PATH}/polyglat-ir.so"
-            --passes=polyglat
-            --polyglat-output="${TARGET_PSM_FILE}"
-            "${ENSEMBLE_BITCODE}"
-            -o "${ENSEMBLE_BITCODE}"
-            COMMAND clang -shared $<TARGET_PROPERTY:${target},LINK_OPTIONS> "${ENSEMBLE_BITCODE}"
+            COMMAND clang -shared $<TARGET_PROPERTY:${target},LINK_OPTIONS> ${LLS}
             -o "$<TARGET_PROPERTY:${target},LIBRARY_OUTPUT_DIRECTORY>/$<TARGET_PROPERTY:${target},PREFIX>${target}$<TARGET_PROPERTY:${target},SUFFIX>"
-            DEPENDS ${BITCODES}
+            DEPENDS ${LLS}
     )
     set_target_properties(${target} PROPERTIES
             LIBRARY_OUTPUT_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
