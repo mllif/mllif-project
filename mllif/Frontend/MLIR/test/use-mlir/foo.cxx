74b6b2d7402f8a260b5d3ab26cfe140439173e7a
Sharp0802 <sharp0802.business@gmail.com>
2025-01-16 03:49:36 +0900

    :white_check_mark: Test all types that can be used by keyword

--- mllif/Frontend/MLIR/test/use-mlir/foo.cxx
+++ mllif/Frontend/MLIR/test/use-mlir/foo.cxx
@@ -1,10 +1,53 @@
-
 namespace ns {
-    struct record {
-        [[mllif::export]] int foo(int a, int b);
+    struct Boo {
+        float A, B;
+    };
+
+    struct Foo {
+        [[mllif::export]] int foo(int a, Boo &b, int c[2]);
+        [[mllif::export]] void chars(
+            char a,
+            char8_t b,
+            char16_t c,
+            char32_t d,
+            wchar_t e);
+        [[mllif::export]] void floats(
+            double a,
+            float b,
+            _Float16 c);
+        [[mllif::export]] void integers(
+            int a,
+            long b,
+            long long c,
+            short d,
+            unsigned int e,
+            unsigned long f,
+            unsigned long long g,
+            unsigned short h);
+        [[mllif::export]] void boolean(bool b);
     };
 }
 
-int ns::record::foo(int a, int b) {
+int ns::Foo::foo(int a, Boo &b, int c[2]) {
     return a;
 }
+void ns::Foo::chars(
+    char a,
+    char8_t b,
+    char16_t c,
+    char32_t d,
+    wchar_t e) {}
+void ns::Foo::floats(
+    double a,
+    float b,
+    _Float16 c) {}
+void ns::Foo::integers(
+    int a,
+    long b,
+    long long c,
+    short d,
+    unsigned int e,
+    unsigned long f,
+    unsigned long long g,
+    unsigned short h) {}
+void ns::Foo::boolean(bool b) {}
