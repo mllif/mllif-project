c5d4d9b47c95dfd0291b1420dda2c4895302dbfb
Sharp0802 <sharp0802.business@gmail.com>
2025-03-31 23:03:39 +0900

    :white_check_mark: Add self-check of sample method

--- mllif/Frontend/C#/test/Program.cs
+++ mllif/Frontend/C#/test/Program.cs
@@ -1,10 +1,30 @@
+using System.Runtime.InteropServices;
+using MLLIF;
+
 namespace MLLIFCSharpFrontTest;
 
-public static class Program
+public partial class Program
 {
     public static int Main()
     {
         Console.WriteLine("Hello, World!");
+
+        var prog = new Program();
+
+        var self = GCHandle.Alloc(prog);
+        var two  = GCHandle.Alloc("2");
+        var r = __mllif__Hello__s32_voidp(GCHandle.ToIntPtr(self), 1, GCHandle.ToIntPtr(two));
+        two.Free();
+        self.Free();
+        
+        Console.WriteLine(r);
+        
         return 0;
+    }
+
+    [Export]
+    public int Hello(int a, string b)
+    {
+        return a + int.Parse(b);
     }
 }
